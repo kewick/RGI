6a8c46f288f59aaee28dd49567b580242ab1e996
arjayaquino <arjayaquino@imagineinfinitystudios.com>
2013-02-17 18:52:24 -0700

    new pagecontroller js file

--- koala-site/js/scripts.js
+++ koala-site/js/scripts.js
@@ -1,25 +1,3 @@
-//BEGIN SHUFFLE FUNCTION
-$.fn.shuffle = function() { 
-        var allElems = this.get(),
-            getRandom = function(max) {
-                return Math.floor(Math.random() * max);
-            },
-            shuffled = $.map(allElems, function(){
-                var random = getRandom(allElems.length),
-                    randEl = $(allElems[random]).clone(true)[0];
-                allElems.splice(random, 1);
-                return randEl;
-           });
- 
-        this.each(function(i){
-            $(this).replaceWith($(shuffled[i]));
-        });
- 
-        return $(shuffled);
- 
-};
-//END SHUFFLE FUNCTION
-
 //BEGIN MODAL BOX FUNCTION
 var modalBox = (function(){
     var 
